b7af04eb109317a9289f2fbd822f1e2bd426987e
Mayank Shrivastava <shrimayank12@gmail.com>
2022-06-19 17:36:00 +0530

    created public and private routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from "react";
-import 'rsuite/dist/styles/rsuite-default.css'
-import './styles/main.scss'
+import React from 'react';
+import { Switch } from 'react-router';
+import 'rsuite/dist/styles/rsuite-default.min.css';
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import './styles/main.scss';
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <Switch>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
   );
 }
 
--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,12 +1,12 @@
-import firebase from 'firebase/app'
+import firebase from 'firebase/app';
 
 const config = {
-    apiKey: "AIzaSyCwbxXmJz4pRx7A9h2SOHxcAIpmandvAUQ",
-    authDomain: "chat-app-2eec1.firebaseapp.com",
-    projectId: "chat-app-2eec1",
-    storageBucket: "chat-app-2eec1.appspot.com",
-    messagingSenderId: "615816183959",
-    appId: "1:615816183959:web:eeafe5d937284fc32ba9d2"
-  };
+  apiKey: 'AIzaSyCwbxXmJz4pRx7A9h2SOHxcAIpmandvAUQ',
+  authDomain: 'chat-app-2eec1.firebaseapp.com',
+  projectId: 'chat-app-2eec1',
+  storageBucket: 'chat-app-2eec1.appspot.com',
+  messagingSenderId: '615816183959',
+  appId: '1:615816183959:web:eeafe5d937284fc32ba9d2',
+};
 
-  const app = firebase.initializeApp (config)
+const app = firebase.initializeApp(config);
