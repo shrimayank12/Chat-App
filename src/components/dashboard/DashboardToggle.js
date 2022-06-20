7591c01e780cb462ee2ca1d305e65c1d9909c15c
Mayank Shrivastava <shrimayank12@gmail.com>
2022-06-20 19:52:55 +0530

    started building the sidebar component

---
+++ src/components/dashboard/DashboardToggle.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Button, Drawer, Icon } from 'rsuite';
+import Dashboard from '.';
+import { useModalState } from '../../misc/custom-hooks';
+
+const DashboardToggle = () => {
+  const { isOpen, open, close } = useModalState();
+
+  return (
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    <>
+      <Button block color="blue" onClick={open}>
+        <Icon icon="dashboard" /> Dashboard
+      </Button>
+      <Drawer show={isOpen} onHide={close} placement="left">
+        <Dashboard />
+      </Drawer>
+    </>
+  );
+};
+
+export default DashboardToggle;
